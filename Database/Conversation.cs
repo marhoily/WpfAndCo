e70a3279225886e855d3db47e258ecb1b083b18c
Ilya <marhoily@hotmail.com>
2016-04-12 21:51:19 +0300

    + Database

---
+++ Database/Conversation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using JetBrains.Annotations;
+
+namespace Dpb.MessageDb
+{
+    [PublicAPI]
+    public class Conversation
+    {
+        private ICollection<Message> _messages;
+
+        [Key] [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Sender { get; set; }
+        [Required, MaxLength(50)]
+        public string Receiver { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public virtual ICollection<Message> Messages
+        {
+            get { return _messages ?? (_messages = new HashSet<Message>()); } 
+        }
+    }
+}
---
+++ Database/Message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using JetBrains.Annotations;
+
+namespace Dpb.MessageDb
+{
+    [PublicAPI]
+    [DebuggerDisplay("{Text} | {Sender} -> {Receiver}")]
+    public class Message
+    {
+        [Key] [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public int Number { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Sender { get; set; }
+        [Required, MaxLength(100)]
+        public string Receiver { get; set; }
+
+        [Required, MaxLength(10*1024)]
+        public string Text { get; set; }
+
+        public DateTime Created { get; set; }
+        public DateTime Saved { get; set; }
+        public DateTime? Read { get; set; }
+
+        public long ConversationId { get; set; }
+        [ForeignKey("ConversationId")]
+        public virtual Conversation Conversation { get; set; }
+    }
+}
