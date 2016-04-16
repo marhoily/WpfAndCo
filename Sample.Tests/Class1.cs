dc1edcf91207536c8468a2264318ee53db126dd3
Ilya <marhoily@hotmail.com>
2016-04-16 22:09:22 +0300

    use modelBuilder

--- Sample.Tests/Class1.cs
+++ Sample.Tests/Class1.cs
@@ -1,4 +1,12 @@
-﻿using Xunit;
+﻿using System.Linq;
+using ApprovalTests;
+using ApprovalUtilities.Utilities;
+using FluentAssertions;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Metadata.Conventions;
+using Microsoft.Data.Entity.Metadata.Conventions.Internal;
+using Xunit;
 
 namespace Sample
 {
@@ -7,7 +15,17 @@ namespace Sample
         [Fact]
         public void FactMethodName()
         {
-
+            var builder = new ModelBuilder(
+                new CoreConventionSetBuilder().CreateConventionSet());
+            builder.Entity<Person>();
+            builder.Entity<City>();
+            IModel ro = builder.Model;
+            var city = ro.FindEntityType(typeof(City).FullName);
+            Approvals.VerifyAll(city.GetProperties()
+                .Select(x =>x.WritePropertiesToString()), "");
+            var property = city.GetProperties().First();
+            property.ClrType.Name.Should().Be("Int64");
+            property.Name.Should().Be("Id");
         }
     }
 }
