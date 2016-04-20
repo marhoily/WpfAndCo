5f3e4ac0c255cdf489b7275a211309bd22aeb227
Ilya <marhoily@hotmail.com>
2016-04-21 00:46:18 +0300

    reafctor

--- Sample.Tests/HierarchyFacts.cs
+++ Sample.Tests/HierarchyFacts.cs
@@ -1,16 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ApprovalTests;
 using Generator;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace Sample
 {
     public class HierarchyFacts
     {
-        private Hierarchy _hierarchy;
-        private const string Dir = @"c:\srcroot\WpfAndCo\Sample.Tests\Sample.Tests.csproj";
-
         public class X
         {
             public IEnumerable<Y> Ys { get { return Enumerable.Empty<Y>();} }
@@ -18,24 +17,18 @@ namespace Sample
 
         public class Y { }
         public class Z { }
-
-        public HierarchyFacts()
+        
+        [Fact]
+        public void Apply()
         {
-            var hierarchy = new HierarchyRoot(Dir, "Generated") {
+            var hierarchy = new HierarchyRoot("c:/dir/my.csproj", "Generated") {
                 new Node<X>(new X()) {
                     new Node<Y> {new Node<Change>()},
                     new Node<Z>()
                 } };
 
-            _hierarchy = hierarchy
-                .With((X m) => m.Ys)
-                .Build();
-        }
-
-        [Fact]
-        public void Apply()
-        {
-
+            Approvals.Verify(JsonConvert.SerializeObject(
+                hierarchy.With((X m) => m.Ys).Build()));
         }
     }
 }
