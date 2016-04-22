04181deb1a9c23b1cc94de779a5a86bfefdd3cff
Ilya <marhoily@hotmail.com>
2016-04-23 01:39:36 +0300

    refactor

--- Generator/Hierarchy/RegNode.cs
+++ Generator/Hierarchy/RegNode.cs
@@ -4,6 +4,8 @@ using System.Linq;
 
 namespace Generator
 {
+    using M = IEnumerable<Tuple<ITransformer, object>>;
+
     public sealed class RegNode
     {
         public Type Tp { get; }
@@ -20,9 +22,26 @@ namespace Generator
         public IEnumerable<NodeExp> Expand(
             List<RegRoot> registrations, object model)
         {
-            return RegModel.Choose(OneArgCtor.From(Tp), Model ?? model, registrations)
-                .Select(o => new NodeExp(o.Transformer, Nodes.SelectMany(
-                    x => x.Expand(registrations, o.Model1))));
+            return Choose(OneArgCtor.From(Tp), Model ?? model, registrations)
+                .Select(o => new NodeExp(o.Item1, Nodes.SelectMany(
+                    x => x.Expand(registrations, o.Item2))));
+        }
+
+        private static M Choose(OneArgCtor ctor,
+            object model, IEnumerable<RegRoot> registrations)
+        {
+            if (ctor.NoArgs) {
+                yield return Tuple.Create(ctor.Invoke(null), model);
+            }
+            else if (ctor.ArgType.IsInstanceOfType(model)) {
+                yield return Tuple.Create(ctor.Invoke(model), model);
+            }
+            else {
+                var reg = registrations.Single(r => r.Value == ctor.ArgType);
+                var args = reg.Convert(model);
+                var many = args.Select(m => Tuple.Create(ctor.Invoke(m), m));
+                foreach (var o in many) yield return o;
+            }
         }
     }
 }
