5ede6d04464568f4d1cdaed6b4b4c9cfbc27e1d5
Ilya <marhoily@hotmail.com>
2016-04-23 22:21:12 +0300

    + sample generator

---
+++ Generator.Tests/ModelGenerator.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace Generator
+{
+    public sealed class ModelGenerator : ITransformer
+    {
+        [Fact]
+        public void Test()
+        {
+            var genHierarchy = Generate(new Model());
+        }
+
+        public static GenHierarchy Generate(Model model)
+        {
+            return new HierarchyBuilder("../../Output/sample.proj", "Generated") {
+                new NodeBuilder<ModelGenerator>(model) {
+                    new NodeBuilder<CompanyGenerator> {
+                        new NodeBuilder<EmployeeGenerator>()
+                    } } }
+                .With((Model m) => m.Companies)
+                .With((Company c) => c.Employees)
+                .Build();
+        }
+
+        public string Name => "model.cs";
+        public string TransformText() => "blah";
+    }
+}
