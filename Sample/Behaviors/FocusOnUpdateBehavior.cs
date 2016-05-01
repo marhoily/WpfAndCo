9ef661b3a3ed7e1783cbecad22a3e609960dc28a
Ilya <marhoily@hotmail.com>
2016-05-02 00:02:33 +0300

    v1 done

---
+++ Sample/Behaviors/FocusOnUpdateBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Alphabet.Behaviors
+{
+    public sealed class FocusOnUpdateBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.DataContextChanged += (s, e) =>
+            {
+                AssociatedObject.Focus();
+                AssociatedObject.SelectAll();
+            };
+        }
+    }
+}
